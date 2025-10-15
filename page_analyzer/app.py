84f658e001eb92b7a17162bcbbdedfbd5fd8de8b
n_popov <npopov2@yandex.com>
2025-10-15 06:42:07 +0300

    added bootstrap

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template
+)
 from dotenv import load_dotenv
 import os
 
@@ -8,4 +11,4 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', text='Hello World!')
